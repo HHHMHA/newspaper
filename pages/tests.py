6432b14d6fe06d62b9da6604f7278241466416db
J2mF <mp4-12cs5@hotmail.com>
2020-09-29 19:12:45 +0300

    Added basic tests
    Main Changes:
    Added tests for routing and templates name
    test for user model

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,19 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
 
 # Create your tests here.
+from django.urls import reverse
+
+
+class HomePageTestS(SimpleTestCase):
+    def test_get_home_page_by_path(self):
+        response = self.client.get('/')
+        self.assertEquals(200, response.status_code)
+
+    def test_get_home_page_by_name(self):
+        response = self.client.get(reverse('home'))
+        self.assertEquals(200, response.status_code)
+
+    def test_right_template_used(self):
+        response = self.client.get(reverse('home'))
+        self.assertEquals(200, response.status_code)
+        self.assertTemplateUsed(response, 'home.html')
--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,29 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 
 # Create your tests here.
+from django.urls import reverse
+
+
+class SignUpTests(TestCase):
+    username = 'newuser'
+    email = 'newuser@gmail.com'
+
+    def test_get_signup_page_by_path(self):
+        response = self.client.get('/users/signup/')
+        self.assertEquals(200, response.status_code)
+
+    def test_get_signup_page_by_name(self):
+        response = self.client.get(reverse('signup'))
+        self.assertEquals(200, response.status_code)
+
+    def test_right_template_used(self):
+        response = self.client.get(reverse('signup'))
+        self.assertEquals(200, response.status_code)
+        self.assertTemplateUsed(response, 'registration/signup.html')
+
+    def test_user_model(self):
+        get_user_model().objects.create_user(self.username, self.email)
+        self.assertEquals(1, get_user_model().objects.count())
+        self.assertEquals(self.username, get_user_model().objects.first().username)
+        self.assertEquals(self.email, get_user_model().objects.first().email)

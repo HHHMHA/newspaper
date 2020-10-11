a4735b5bc53e3084d129c621dc314b0162bb860e
Al-Hasan Saleh <alhasan.saleh@kuwaitnet.com>
2020-10-11 11:29:20 +0300

    Replaced Custom AddUserToKwargsMixin with braces

--- articles/mixins.py
+++ articles/mixins.py
@@ -7,11 +7,3 @@ class UserIsArticleAuthorMixin(AccessMixin):
         if self.get_object().author != request.user:
             return self.handle_no_permission()
         return super().dispatch(request, *args, **kwargs)
-
-
-class AddUserToKwargsMixin:
-    """Adds the user who sent the request to the kwargs so it can be accessed outside for example in forms"""
-    def get_form_kwargs(self):
-        kwargs = super(AddUserToKwargsMixin, self).get_form_kwargs()
-        kwargs['user'] = self.request.user
-        return kwargs
--- newspaper/settings.py
+++ newspaper/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     # 3rd party
     'crispy_forms',
     'floppyforms',
+    'braces',
 
     # Custom
     'users.apps.UsersConfig',

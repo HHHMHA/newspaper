9dfa9822d380aa767aa3392b3d8a93e9429d7da2
Al-Hasan Saleh <alhasan.saleh@kuwaitnet.com>
2020-10-11 08:36:52 +0300

    Refactor Adding user to kwargs in a custom mixin
    Main Changes:
    Update requirements.txt
    Add django-floppyforms
    Rename UserIsArticleCreator to UserIsArticleCreatorMixin

--- articles/mixins.py
+++ articles/mixins.py
@@ -1,9 +1,17 @@
 from django.contrib.auth.mixins import AccessMixin
 
 
-class UserIsArticleAuthor(AccessMixin):
+class UserIsArticleAuthorMixin(AccessMixin):
     """Deny a request with a permission error if the user who sent the request is not the author of the article."""
     def dispatch(self, request, *args, **kwargs):
         if self.get_object().author != request.user:
             return self.handle_no_permission()
         return super().dispatch(request, *args, **kwargs)
+
+
+class AddUserToKwargsMixin:
+    """Adds the user who sent the request to the kwargs so it can be accessed outside for example in forms"""
+    def get_form_kwargs(self):
+        kwargs = super(AddUserToKwargsMixin, self).get_form_kwargs()
+        kwargs['user'] = self.request.user
+        return kwargs
--- newspaper/settings.py
+++ newspaper/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     # 3rd party
     'crispy_forms',
+    'floppyforms',
 
     # Custom
     'users.apps.UsersConfig',
